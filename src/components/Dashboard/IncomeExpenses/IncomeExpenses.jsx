b7c43663e5b9f0e03b9567b87cdadcd14dcd5cda
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-05-30 22:35:46 +0200

    update

--- src/components/Dashboard/IncomeExpenses/IncomeExpenses.jsx
+++ src/components/Dashboard/IncomeExpenses/IncomeExpenses.jsx
@@ -1,19 +1,35 @@
+// @ts-nocheck
 import React from 'react';
 import { useDashboard } from "../../../hooks/useDashboard";
+import { Table } from '../RecentCustomers/RecentCustomers.styled';
+import { TDamount, TDname, TDtype, TH } from './IncomeExpenses.styled';
 
 const IncomeExpenses = () => {
 const { incomeExpenses } = useDashboard();
 
   return (
     <section>
-      <table>
+      <Table>
         <caption>Income/Expenses</caption>
         <thead>
             <tr>
-                <th>Today</th>
+                <TH>Today</TH>
+                <TH>{" "}</TH>
+                <TH>{" "}</TH>
             </tr>
         </thead>
-      </table>
+        <tbody>
+         {incomeExpenses.map((item, index) => {
+            return (
+              <tr key={index}>
+                <TDtype itemtype={item.type.toString()}><p>{item.type}</p></TDtype>
+                <TDname>{item.name}</TDname>
+                <TDamount itemtype={item.type.toString()}>{item.amount}</TDamount>
+              </tr>
+            );
+         })}
+        </tbody>
+      </Table>
     </section>
   )
 }
---
+++ src/components/Dashboard/IncomeExpenses/IncomeExpenses.styled.jsx
@@ -0,0 +1,68 @@
+// @ts-nocheck
+import styled from "@emotion/styled";
+
+export const TH = styled.th`
+  border-bottom: 1px solid var(--border-color);
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.29;
+  color: var(--placeholder-color);
+  padding: 14px 0;
+`;
+
+export const TDtype = styled.td`
+  border-bottom: 1px solid var(--border-color);
+
+  p {
+    background: ${({ itemtype }) =>
+      itemtype === "Expense"
+        ? "var(--expense-bg)"
+        : itemtype === "Error"
+        ? "var(--border-color)"
+        : "var(--income-bg)"};
+    color: ${({ itemtype }) =>
+      itemtype === "Expense"
+        ? "var(--expense-color)"
+        : itemtype === "Error"
+        ? "var(--main-black)"
+        : "var(--accent-color)"};
+    border-radius: 40px;
+    width: 80px;
+    height: 23px;
+    min-height: 23px;
+    min-width: 80px;
+    font-weight: 500;
+    font-size: 12px;
+    letter-spacing: -0.05em;
+    text-align: center;
+    padding-top: 4px;
+  }
+`;
+
+export const TDname = styled.td`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 1.29;
+  color: var(--main-black);
+  text-align: left;
+  padding-left: 14px;
+  padding-top: 14px;
+  padding-bottom: 14px;
+  border-bottom: 1px solid var(--border-color);
+`;
+
+export const TDamount = styled.td`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 1.29;
+  color: ${({ itemtype }) =>
+    itemtype === "Expense"
+      ? "var(--expense-color)"
+      : itemtype === "Error"
+      ? "var(--main-black)"
+      : "var(--accent-color)"};
+  text-align: right;
+  text-decoration: ${({ itemtype }) =>
+    itemtype === "Error" ? "line-through" : "none"};
+  border-bottom: 1px solid var(--border-color);
+`;
