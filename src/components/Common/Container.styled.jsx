d093eb18ee4073ba41576355217ebb5036d75bc0
VladyslavVagin <vladtwince@gmail.com>
2024-07-29 13:36:55 +0200

    update bg-image

--- src/components/Common/Container.styled.jsx
+++ src/components/Common/Container.styled.jsx
@@ -9,18 +9,19 @@ export const Container = styled.section`
   margin: 0 auto;
   background-image: url(${bgImg});
   background-repeat: no-repeat;
-  background-position: 135px 536px;
+  background-position: 135px 120%;
+  background-size: contain;
 
   @media screen and (min-width: 768px) {
     max-width: 768px;
     padding: 0 32px 30vh 32px;
-    background-position: 484px 667px;
+    background-position: 375px 120%;
   }
 
   @media screen and (min-width: 1440px){
     max-width: 1440px;
     padding: 0 100px 30vh 100px;
-    background-position: 1142px 704px;
+    background-position: 720px 45dvh;
   }
 
   @media (min-device-pixel-ratio: 2),
